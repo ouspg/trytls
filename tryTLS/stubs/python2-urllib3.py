f759dd6caca762d960bf03c28e00816e757dcb34
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-06-21 12:52:57 +0300

    changed path

--- tryTLS/stubs/python2-urllib3.py
+++ tryTLS/stubs/python2-urllib3.py
@@ -2,7 +2,7 @@ import urllib3
 
 
 http = urllib3.PoolManager(                                                        cert_reqs='CERT_REQUIRED', # Force certificate check.
-    ca_certs='/etc/ssl/certs/ca-bundle.crt'
+    ca_certs='/etc/ssl/certs/ca-certificates.crt'
 )
 
 #test = ['expired', 'wrong.host', 'self-signed', 'sha1-2016', 'sha1-2017', 'sha256', '1000-sans', '10000-sans', 'incomplete-chain', 'rsa8192', 'mozilla-old', 'mozilla-intermediate', 'mozilla-modern', 'dh480', 'dh1024', 'dh2048', 'dh-small-subgroup', 'dh-composite', 'superfish', 'edellroot', 'dsdtestprovider']
