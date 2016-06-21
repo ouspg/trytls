7fa049bd1787404626aec013d70216ac202298fa
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-06-21 12:40:35 +0300

    Added url prefixes

--- tryTLS/stubs/python2-urllib3.py
+++ tryTLS/stubs/python2-urllib3.py
@@ -5,8 +5,38 @@ http = urllib3.PoolManager(                                                     
     ca_certs='/etc/ssl/certs/ca-bundle.crt'
 )
 
-test = ['expired', 'wrong.host', 'self-signed', 'sha1-2016', 'sha1-2017', 'sha256', '1000-sans', '10000-sans', 'incomplete-chain', 'rsa8192', 'mozilla-old', 'mozilla-intermediate', 'mozilla-modern', 'dh480', 'dh1024', 'dh2048', 'dh-small-subgroup', 'dh-composite', 'superfish', 'edellroot', 'dsdtestprovider']
+#test = ['expired', 'wrong.host', 'self-signed', 'sha1-2016', 'sha1-2017', 'sha256', '1000-sans', '10000-sans', 'incomplete-chain', 'rsa8192', 'mozilla-old', 'mozilla-intermediate', 'mozilla-modern', 'dh480', 'dh1024', 'dh2048', 'dh-small-subgroup', 'dh-composite', 'superfish', 'edellroot', 'dsdtestprovider']
 
+test = [
+    "expired",
+    "wrong.host",
+    "self-signed",
+    "sha256",
+    "1000-sans",
+    "10000-sans",
+    "incomplete-chain",
+    "rsa8192",
+    "cbc",
+    "rc4",
+    "mozilla-old",
+    "mozilla-intermediate",
+    "mozilla-modern",
+    "dh480",
+    "dh1024",
+    "dh2048",
+    "dh-small-subgroup",
+    "dh-composite",
+    "hsts",
+    "upgrade",
+    "preloaded-hsts",
+    "subdomain.preloaded-hsts",
+    "http",
+    "http-password",
+    "pinning-test",
+    "superfish",
+    "edellroot",
+    "dsdtestprovider"
+]
 
 for t in test:
     try:
@@ -15,6 +45,3 @@ for t in test:
         print "%s failed" % t, e
     else:
         print "%s pass" % t
-
-
-    
