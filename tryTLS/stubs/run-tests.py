eeea94e9494a40b31796e86186e5d87fdf7849eb
Pekka Pietikäinen <pekka@pietikainen.org>
2016-06-21 13:24:12 +0300

    Test stub as external process

---
+++ tryTLS/stubs/run-tests.py
@@ -0,0 +1,45 @@
+import subprocess
+
+test = [
+    "expired",
+    "wrong.host",
+    "self-signed",
+    "sha256",
+    "1000-sans",
+    "10000-sans",
+    "incomplete-chain",
+    "rsa8192",
+    "cbc",
+    "rc4",
+    "mozilla-old",
+    "mozilla-intermediate",
+    "mozilla-modern",
+    "dh480",
+    "dh1024",
+    "dh2048",
+    "dh-small-subgroup",
+    "dh-composite",
+    "hsts",
+    "upgrade",
+    "preloaded-hsts",
+    "subdomain.preloaded-hsts",
+    "http",
+    "http-password",
+    "pinning-test",
+    "superfish",
+    "edellroot",
+    "dsdtestprovider"
+]
+
+
+for t in test:
+    url = "https://%s.badssl.com/" % t
+    try:
+        subprocess.check_output(["/usr/bin/python","python-urllib3.py",url], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print("FAIL: %s (%s)" % (url,e.output))
+    else:
+        print("PASS: %s" % url)
+        
+
+
