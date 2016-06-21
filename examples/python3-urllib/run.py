82826f468186b737d63dffb2c79cfeff5a8d47a0
Joachim Viide <jviide@iki.fi>
2016-06-22 01:55:35 +0300

    Add a Python3 + urllib example

---
+++ examples/python3-urllib/run.py
@@ -0,0 +1,17 @@
+import sys
+import ssl
+import urllib.error
+import urllib.request
+
+host = sys.argv[1]
+port = sys.argv[2]
+cafile = sys.argv[3] if len(sys.argv) > 3 else None
+
+try:
+    urllib.request.urlopen("https://" + host + ":" + port, cafile=cafile)
+except urllib.error.URLError as exc:
+    if not isinstance(exc.reason, ssl.SSLError):
+        raise
+    print("FAIL")
+else:
+    print("OK")
