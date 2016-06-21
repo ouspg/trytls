bf34afe27954148131e5f0f32bf4c6274dde185c
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-06-21 13:29:29 +0000

    Attempted to enable lua https verification

--- tryTLS/stubs/lua-https.lua
+++ tryTLS/stubs/lua-https.lua
@@ -1,16 +1,19 @@
-local https = require 'ssl.https'
+local https = require "ssl.https"
 
 function DoThings()
   if arg[1]==nil then
     print(string.format( "Usage: %s <URL>", arg[1] ))
-    break
   else
     local r, c, h, s = https.request{
       url = tostring(arg[1]),
       sink = ltn12.sink.table({}),
-      protocol = "tlsv1"
+      protocol = "tlsv1",
+      mode = "client",
+      cafile = "/etc/ssl/certs/Sonera_Class_1_Root_CA.pem",
+      verify = "peer",
+      options = "all"
     }
-    print(r,c,h,s)
+    print(arg[1],c)
   end
 end
 DoThings()
