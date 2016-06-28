91d01ab0c29c19b4b50f8b6ba6105fe114acc2ab
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-06-28 08:59:35 +0000

    newest version

--- tryTLS/stubs/lua-https.lua
+++ tryTLS/stubs/lua-https.lua
@@ -1,19 +1,21 @@
 local https = require "ssl.https"
 
 function DoThings()
-  if arg[1]==nil then
-    print(string.format( "Usage: %s <URL>", arg[1] ))
-  else
-    local r, c, h, s = https.request{
-      url = tostring(arg[1]),
-      sink = ltn12.sink.table({}),
-      protocol = "tlsv1",
-      mode = "client",
-      cafile = "/etc/ssl/certs/",
-      verify = "peer",
-      options = "all"
-    }
-    print(arg[1],c)
-  end
+    if arg[1]==nil then
+        print(string.format( "Usage: %s <URL>", arg[1] ))
+    else
+        local r, c, h, s = https.request{
+            url = tostring(arg[1]),
+            options = "all",
+            --sink = ltn12.sink.table({}),
+            protocol = "tlsv1",
+            mode = "client",
+            cafile = "/etc/ssl/certs/ca-certificates.crt",
+            verify = "peer"
+            --key = "/root/client.key",
+            --certificate="/root/client.crt"
+        }
+        print(arg[1],c)
+    end
 end
 DoThings()
