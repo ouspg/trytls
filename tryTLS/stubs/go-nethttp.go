d2e6be55a03c34d4594ea3924a3168532a7c72a0
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-06-28 17:56:02 +0300

    Add go http client stub

---
+++ tryTLS/stubs/go-nethttp.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func usage() {
+	fmt.Printf("Usage: %v <URL>", os.Args[0])
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+		return
+	}
+	url := os.Args[1]
+
+	// Perform a HTTP Request
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
