a69145bcea91be14f454ed03a6987091a629c9d9
Joachim Viide <jviide@iki.fi>
2016-06-29 15:15:53 +0300

    Bump the version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="trytls",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["trytls=showrunner.runner:main"]
