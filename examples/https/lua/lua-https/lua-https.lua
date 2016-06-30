51ee6312f239996ed42a3f4fb7bcb3382e8bb157
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-06-30 10:40:59 +0000

    Yes

--- examples/https/lua/lua-https/lua-https.lua
+++ examples/https/lua/lua-https/lua-https.lua
@@ -1,19 +1,36 @@
 local https = require "ssl.https"
+local socket = require "socket"
+local ssl = require "ssl"
 
-function DoThings()
-  if arg[1]==nil then
-    print(string.format( "Usage: %s <URL>", arg[1] ))
-  else
-    local r, c, h, s = https.request{
-      url = tostring(arg[1]),
-      sink = ltn12.sink.table({}),
-      protocol = "tlsv1",
-      mode = "client",
-      cafile = "/etc/ssl/certs/",
-      verify = "peer",
-      options = "all"
-    }
-    print(arg[1],c)
-  end
+function tablelength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
 end
-DoThings()
+
+function DoOtherThings()
+    if tablelength(arg)<4 then
+        print(string.format( "Usage: %s <URL> <PORT> (<CA-BUNDLE>)", arg[0] ))
+    else
+        local params = {
+            mode = "client",
+            protocol = "tlsv1",
+            verify = "peer",
+            options = "all",
+            cafile = "/etc/ssl/certs/ca-certificates.crt" or arg[3],
+        }
+
+        local conn = socket.tcp()
+        conn:connect(tostring(arg[1]), arg[2])
+
+        conn = ssl.wrap(conn, params)
+        conn:dohandshake()
+
+        conn:send("GET / HTTP/1.1\n\n")
+        local line, err = conn:receive()
+        print(arg[1], err, line)
+        conn:close()
+    end
+end
+
+DoOtherThings()
