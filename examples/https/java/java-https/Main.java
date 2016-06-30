b81d956a8bca5c7369ccbd83de5146f7ba5ae7da
aleksiklasila <aleksi.klasila35@gmail.com>
2016-06-30 12:25:47 +0300

    edited java Calling convention

--- examples/https/java/java-https/Main.java
+++ examples/https/java/java-https/Main.java
@@ -6,30 +6,37 @@ public class Main{
 	
 	public static void main(String[] args)
 	{
-		try (BufferedReader br = new BufferedReader(new FileReader("tmp/messages"))) {
-			String line;
-			String splitted[]; // = port & message & crt & hostname
-			URL url;
-			int port = 0, message = 1, hostname = 3;
-			while ((line = br.readLine()) != null) {
-				splitted = line.split(" & ", 4);
-				String https_url = String.format("https://%s:%s", splitted[hostname], splitted[port]);
-				try {
-					url = new URL(https_url);
-					HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
-					if (con.getResponseCode() == 200){
-						System.out.println("    True: " + splitted[message]);
-					} else {
-						System.out.println("not True: " + splitted[message]);
-					}
-				} catch (Exception e) {
-					System.out.println("not True: " + splitted[message]);
-				}
-			}
-		} catch (Exception e) {
-			return ;
+
+		if (args.length < 2)
+			System.exit(1);
+
+		String host = args[0];
+		String port = args[1];
+		String ca_bundle = "";
+		
+		if (args.length > 3) {
+			ca_bundle = args[3]; 
+			System.out.println("UNSUPPORTED");	//for now
+			System.exit(2);
 		}
-	}
+
+		URL url;
+		String https_url = String.format("https://%s:%s", host, port);
+		try {
+			url = new URL(https_url);
+			HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+			
+			if (con.getResponseCode() == 200)
+				System.out.println("VERIFY SUCCESS");
+			else
+				System.out.println("VERIFY FAILURE");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.exit(3);
+		}
+
+		System.exit(0);
+	} 
 	
 }
 
