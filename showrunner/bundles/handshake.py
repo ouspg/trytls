4ce0c90323a22c2910455026455c8f23d0ba6a1f
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-06-30 23:08:45 +0300

    Added missing badssl tests
    Left the explanations as comments (helpful?). Also, there were a couple of "maybes" in there:
        #badssl(False, "sha1-2016"), maybe FAIL after 2016
        #badssl(False, "sha1-2017"), maybe FAIL after 2017
        #badssl(True, "rsa8192"), future proofing
    These would require more if:s so assumed WIP/obsolete

--- showrunner/bundles/handshake.py
+++ showrunner/bundles/handshake.py
@@ -1,9 +1,18 @@
 from ..testenv import badssl, local
 
 
-badssl_tests = [
-    badssl(True, "sha1-2016"),
-    badssl(False, "expired")
+badssl_tests = [    
+    badssl(False, "expired"), # not with an obsolete cert
+    badssl(False, "wrong.host"), # not with a wrong name
+    badssl(False, "self-signed"), # not just one's own claims
+    badssl(True, "sha256"), # future proof sha
+    badssl(True, "1000-sans"), # massive alternative names
+    badssl(True, "10000-sans"), # massive alternative names
+    badssl(False, "incomplete-chain"), # should have full proof of chain to trusted CA
+    badssl(False, "pinning-test"), # why???
+    badssl(False, "superfish"), # super fishy CA
+    badssl(False, "edellroot"), # rotten roots CA
+    badssl(False, "dsdtestprovider") # unproviding CA
 ]
 
 
