fc0ced9131f0ee6b73cc372cfcdef0d36a2d343d
Pekka Pietikäinen <pekka@pietikainen.org>
2016-06-30 15:03:44 +0300

    .NET Core 1.0 version

---
+++ examples/https/dotnet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		try {
+			GetURL(args[0]).Wait();
+			Console.WriteLine("PASS");
+		} catch (AggregateException ae) {
+			ae.Handle((x) =>
+			{
+			if (x is HttpRequestException) 
+			{
+				Console.WriteLine("FAIL {0}",x.Message);
+				return true;
+			}
+			else {
+				return false;
+			}
+			});
+		} catch (System.IndexOutOfRangeException) {
+			Console.WriteLine("Usage: dotnettest <URL>");
+		} catch (Exception e) {
+			Console.WriteLine("MEGAFAIL {0} {1}",e.Message,e);
+		} 
+	}
+	
+	static async Task GetURL(string url)
+	{
+		using (HttpClient client = new HttpClient())
+		using (HttpResponseMessage response = await client.GetAsync(url))
+		using (HttpContent content = response.Content)
+		{
+		// ... Read the string.
+			string result = await content.ReadAsStringAsync();
+
+		}
+	}	
+}
+
+
