96b6cc5c34625b75e4e60c52c6a576cab1f6e466
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-07-01 08:45:33 +0000

    some changes apparently

--- examples/https/lua/lua-https/lua-https.lua
+++ examples/https/lua/lua-https/lua-https.lua
@@ -7,20 +7,46 @@ function tablelength(T)
     for _ in pairs(T) do count = count + 1 end
     return count
 end
-
+function DoThings()
+    if tablelength(arg)<4 then
+        print(string.format( "Usage: %s <URL> <PORT> (<CA-BUNDLE>)", arg[0] ))
+    else
+        local cert = "/etc/ssl/certs/ca-certificates.crt"
+        if not arg[3]==nil then
+            cert = arg[3]
+        end
+        print(cert)
+        local body, code, headers, status = https.request({
+            url = arg[1],
+            mode = "client",
+            protocol = "tlsv1",
+            verify = "peer",
+            options = "all",
+            cafile = cert,
+        })
+        print(body,code,headers,status)
+    end
+end
 function DoOtherThings()
     if tablelength(arg)<4 then
         print(string.format( "Usage: %s <URL> <PORT> (<CA-BUNDLE>)", arg[0] ))
     else
+        local cert = "/etc/ssl/certs/ca-certificates.crt"
+        if not arg[3]==nil then
+            cert = arg[3]
+        end
+        print(arg[1], arg[2], arg[3])
+        print(cert)
         local params = {
             mode = "client",
             protocol = "tlsv1",
             verify = "peer",
             options = "all",
-            cafile = "/etc/ssl/certs/ca-certificates.crt" or arg[3],
+            cafile = cert,
         }
 
         local conn = socket.tcp()
+        conn=net.createConnection(net.TCP, false)
         conn:connect(tostring(arg[1]), arg[2])
 
         conn = ssl.wrap(conn, params)
@@ -28,9 +54,10 @@ function DoOtherThings()
 
         conn:send("GET / HTTP/1.1\n\n")
         local line, err = conn:receive()
-        print(arg[1], err, line)
+        print(err or line)
         conn:close()
     end
 end
 
 DoOtherThings()
+--DoThings()
