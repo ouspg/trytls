53853c8f3b459a4ac26a85c4e2862def842b199c
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-07-01 13:19:27 +0300

    updated and modified from master

--- showrunner/bundles/https.py
+++ showrunner/bundles/https.py
@@ -11,19 +11,19 @@ def https_callback(conn, certfile, keyfile):
 local = functools.partial(_local, callback=https_callback)
 
 
-badssl_tests = [    
-    badssl(False, "expired"), # not with an obsolete cert
-    badssl(False, "wrong.host"), # not with a wrong name
-    badssl(False, "self-signed"), # not just one's own claims
-    badssl(True, "sha256"), # future proof sha
-    badssl(True, "1000-sans"), # massive alternative names
-    badssl(True, "10000-sans"), # massive alternative names
-    badssl(False, "incomplete-chain"), # should have full proof of chain to trusted CA
-    badssl(False, "pinning-test"), # why???
-    badssl(False, "superfish"), # super fishy CA
-    badssl(False, "edellroot"), # rotten roots CA
-    badssl(False, "dsdtestprovider") # unproviding CA
-  ]
+badssl_tests = [
+    badssl(False, "expired"),  # not with an obsolete cert
+    badssl(False, "wrong.host"),  # not with a wrong name
+    badssl(False, "self-signed"),  # not just one's own claims
+    badssl(True, "sha256"),  # future proof sha
+    badssl(True, "1000-sans"),  # massive alternative names
+    badssl(True, "10000-sans"),  # massive alternative names
+    badssl(False, "incomplete-chain"),  # should have full proof of chain to trusted CA
+    badssl(False, "pinning-test"),  # why???
+    badssl(False, "superfish"),  # super fishy CA
+    badssl(False, "edellroot"),  # rotten roots CA
+    badssl(False, "dsdtestprovider")  # unproviding CA
+]
 
 
 local_tests = [
