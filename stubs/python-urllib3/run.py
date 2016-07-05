b3d13978af89080b56db990475088f2298b0aac7
Mauri Miettinen <mauri.miettinen13@gmail.com>
2016-07-05 15:15:10 +0300

    updated python stub

--- stubs/python-urllib3/run.py
+++ stubs/python-urllib3/run.py
@@ -1,16 +1,21 @@
 import urllib3
 import sys
 
+if len(sys.argv) < 2:
+    exit("Usage: %s <URL> <PORT> [CA_FILE]" % sys.argv[0])
+
+cert = None
+if len(sys.argv) >= 3:
+    cert = sys.argv[3]
+
 http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',  # Force certificate check.
-    ca_certs='/etc/ssl/certs/ca-bundle.crt'
+    ca_certs=cert
 )
 
-if len(sys.argv) < 2:
-    exit("Usage: %s <URL>" % sys.argv[0])
 try:
-    r = http.request('GET', sys.argv[1])
+    r = http.request('GET', "https://{}:{}".format(sys.argv[1], sys.argv[2]))
 except Exception as e:
-    exit("%s" % e)
+    print("VERIFY FAILURE")
 else:
-    exit(0)
+    print("VERIFY SUCCESS")
