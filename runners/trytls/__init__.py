97a1419e53699d339090ce25da0c0e36eb316595
Joachim Viide <jviide@iki.fi>
2016-07-11 22:24:16 +0300

    Publish trytls tool version as trytls.__version__

--- runners/trytls/__init__.py
+++ runners/trytls/__init__.py
@@ -1,3 +1,5 @@
 from .testenv import testenv
 
+__version__ = "0.0.6"
+
 __all__ = ["testenv"]
--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
+import os
+import imp
 import sys
 from setuptools import setup, find_packages
 
-
-def format_version(version_info):
-    return ".".join(str(x) for x in version_info[:3])
-
+# Check the the Python version
 if sys.version_info < (3,):
     py_required = (2, 7, 9)
 else:
     py_required = (3, 4, 0)
 
 if sys.version_info < py_required:
+    def format_version(version_info):
+        return ".".join(str(x) for x in version_info[:3])
+
     sys.exit("ERROR: Python {0} or later required (you have {1})".format(
         format_version(py_required),
         format_version(sys.version_info)
     ))
 
+# Import the local version of the package
+found = imp.find_module("trytls", [os.path.join(os.path.dirname(__file__), "runners")])
+trytls = imp.load_module("trytls", *found)
+
 setup(
     name="trytls",
-    version="0.0.6",
+    version=trytls.__version__,
     package_dir={"": "./runners"},
     packages=find_packages("./runners"),
     entry_points={
