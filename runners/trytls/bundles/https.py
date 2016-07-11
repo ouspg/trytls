f882b7b74c6d8a1999748c34436e0cf90735d231
Joachim Viide <jviide@iki.fi>
2016-07-12 01:12:52 +0300

    Use BaseHTTPServer/http.server in trytls.bundles.https

--- runners/trytls/bundles/https.py
+++ runners/trytls/bundles/https.py
@@ -1,14 +1,85 @@
 import ssl
-import functools
-from ..testenv import constant, badssl, badssl_onlymyca, local as _local
+import sys
+import socket
+import contextlib
+import multiprocessing
+from ..utils import tmpfiles
+from ..gencert import gencert
+from ..testenv import testenv, constant
+
+try:
+    from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+except ImportError:
+    from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-def https_callback(conn, certfile, keyfile):
-    s = ssl.wrap_socket(conn, server_side=True, certfile=certfile, keyfile=keyfile)
-    s.sendall(b"HTTP/1.0 200 OK\r\nContent-Length: 0\r\n\r\n")
+def badssl(ok_expected, name):
+    return constant(ok_expected, name + ".badssl.com", 443)
 
 
-local = functools.partial(_local, callback=https_callback)
+@testenv
+def badssl_onlymyca(ok_expected, name):
+    _, _, cadata = gencert("localhost")
+    with tmpfiles(cadata) as cafile:
+        yield ok_expected, name + ".badssl.com", 443, cafile
+
+
+@contextlib.contextmanager
+def http_server(certdata, keydata, host="localhost", port=0):
+    class Server(HTTPServer):
+        ALLOWED_EXCEPTIONS = (socket.error,)
+
+        def handle_error(self, request, client_address):
+            exc_type, _, _ = sys.exc_info()
+            if isinstance(exc_type, type) and issubclass(exc_type, self.ALLOWED_EXCEPTIONS):
+                return
+            HTTPServer.handle_error(self, request, client_address)
+
+    class Handler(BaseHTTPRequestHandler):
+        def setup(self):
+            with tmpfiles(certdata, keydata) as (certfile, keyfile):
+                self.request = ssl.wrap_socket(
+                    self.request,
+                    server_side=True,
+                    certfile=certfile,
+                    keyfile=keyfile
+                )
+            return BaseHTTPRequestHandler.setup(self)
+
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header("Content-Type", "0")
+            self.end_headers()
+
+        def log_message(self, format, *args):
+            pass
+
+    def serve(connection, certdata, keydata, host, port):
+        server = Server((host, port), Handler)
+        connection.send((host, server.server_port))
+        server.handle_request()
+
+    reader, writer = multiprocessing.Pipe(duplex=False)
+    process = multiprocessing.Process(
+        target=serve,
+        args=[writer, certdata, keydata, host, port]
+    )
+    process.start()
+    try:
+        host, port = reader.recv()
+        yield host, port
+    finally:
+        process.terminate()
+        process.join()
+
+
+@testenv
+def local(ok_expected, cn):
+    certdata, keydata, cadata = gencert(cn)
+
+    with http_server(certdata, keydata) as (host, port):
+        with tmpfiles(cadata) as cafile:
+            yield ok_expected, host, port, cafile
 
 
 badssl_tests = [
