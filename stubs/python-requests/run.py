45698aeb4f7c37e72b3dd7950d00bc3cebd331f5
Jani Kenttälä <janike@iki.fi>
2016-07-12 15:49:37 +0300

    python-requests - use verify argument instead of cert
    According to the documentation, "cert" parameter is used for client auth.
    http://docs.python-requests.org/en/master/user/advanced/
    
    Thanks @oherrala for spotting this.

--- stubs/python-requests/run.py
+++ stubs/python-requests/run.py
@@ -4,10 +4,10 @@ import requests
 
 host = sys.argv[1]
 port = sys.argv[2]
-cafile = sys.argv[3] if len(sys.argv) > 3 else False
+cafile = sys.argv[3] if len(sys.argv) > 3 else True
 
 try:
-    r = requests.get("https://" + host + ":" + port, cert=cafile)
+    r = requests.get("https://" + host + ":" + port, verify=cafile)
 except requests.exceptions.SSLError as err:
     #    print err
     print ("VERIFY FAILURE")
