dcd05bbda982de4904d01bc9452ebe26c81cf1cc
Jani Kenttälä <janike@iki.fi>
2016-07-12 16:13:33 +0300

    python-requests - formatting and other cleanup

--- stubs/python-requests/README.md
+++ stubs/python-requests/README.md
@@ -4,9 +4,9 @@ This stub has been tested with:
  * Python 2.7.12
  * [requests-library](http://docs.python-requests.org/en/master/) v.2.10
 
- # Examples
+# Examples
 
- ```
+```sh
 $ python stubs/python-requests/run.py sha256.badssl.com 443
 VERIFY SUCCESS
 
@@ -15,7 +15,6 @@ VERIFY FAILURE
 
 $ python stubs/python-requests/run.py sha256.badssl.com 443 <path>/pki/certs/theonlycertitrust.crt
 VERIFY FAILURE
-
 ```
 
 # Credits
--- stubs/python-requests/run.py
+++ stubs/python-requests/run.py
@@ -1,15 +1,13 @@
 import sys
 import requests
 
-
 host = sys.argv[1]
 port = sys.argv[2]
-cafile = sys.argv[3] if len(sys.argv) > 3 else True
+verify = sys.argv[3] if len(sys.argv) > 3 else True
 
 try:
-    r = requests.get("https://" + host + ":" + port, verify=cafile)
+    r = requests.get("https://" + host + ":" + port, verify=verify)
 except requests.exceptions.SSLError as err:
-    #    print err
     print ("VERIFY FAILURE")
 else:
     print("VERIFY SUCCESS")
