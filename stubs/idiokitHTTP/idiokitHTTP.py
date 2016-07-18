8baf1a7e018bc1dbd78b81334bdddcb48c3683ce
Timo Mattila <timattil@abusesa.com>
2016-07-18 09:29:18 +0300

    Update idiokitHTTP.py
    Removed catfile and else: pass.

--- stubs/idiokitHTTP/idiokitHTTP.py
+++ stubs/idiokitHTTP/idiokitHTTP.py
@@ -12,17 +12,11 @@ def client(host, port):
     ssl.match_hostname(cert, host)
 
 
-#<host> <port> [ca-bundle]                                                                 
-
-
 host = sys.argv[1]
 port = sys.argv[2]
-catfile = sys.argv[3] if len(sys.argv) > 3 else None
 
 try:
     idiokit.main_loop(client(host, int(port)))
     print "VERIFY SUCCESS"
 except idiokit.ssl.SSLError:
     print "VERIFY FAILURE"
-else:
-    pass
