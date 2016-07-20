4987695505f2be58c79f5c1e693eda6bb37348c0
aleksiklasila <aleksi.klasila35@gmail.com>
2016-07-20 11:44:56 +0300

    Update run.cs

--- stubs/cSharp-Net/run.cs
+++ stubs/cSharp-Net/run.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Net;
-//tested with Mono and Visual Studio:
-//*  Mono: can't recommend using, test if you do not believe
-//*  Visual Studio (all Ok). Same for F# and Visual Basic Stubs
 
 public class Run
 {
