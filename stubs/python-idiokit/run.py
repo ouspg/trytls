805fca381f6b4c34349f96631e7b85b81eb2013e
aleksiklasila <aleksi.klasila35@gmail.com>
2016-07-21 14:13:34 +0300

    python-idiokit: support for cafiles

--- stubs/python-idiokit/run.py
+++ stubs/python-idiokit/run.py
@@ -3,23 +3,25 @@ import idiokit
 from idiokit import socket, ssl
 
 @idiokit.stream
-def client(host, port):
+def client(host, port, cafile):
     sock = socket.Socket()
     yield sock.connect((host, port))
 
-    ssl_sock = yield ssl.wrap_socket(sock, require_cert=True)
+    if cafile:
+        ssl_sock = yield ssl.wrap_socket(sock, require_cert=True, ca_certs=cafile)
+    else:
+        ssl_sock = yield ssl.wrap_socket(sock, require_cert=True)
     cert = yield ssl_sock.getpeercert()
     ssl.match_hostname(cert, host)
 
 host = sys.argv[1]
 port = sys.argv[2]
-
-if len(sys.argv) > 3:
-    print "UNSUPPORTED"
-    sys.exit(0)
+cafile = sys.argv[3] if len(sys.argv) > 3 else None
 
 try:
-    idiokit.main_loop(client(host, int(port)))
+    idiokit.main_loop(client(host, int(port), cafile))
     print "VERIFY SUCCESS"
 except idiokit.ssl.SSLError:
     print "VERIFY FAILURE"
+else:
+    pass
