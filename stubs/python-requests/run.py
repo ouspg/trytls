16992a03616b7853636b490057ab6fd8c52e4b51
aleksiklasila <aleksi.klasila35@gmail.com>
2016-07-26 14:44:58 +0300

    Update run.py

--- stubs/python-requests/run.py
+++ stubs/python-requests/run.py
@@ -2,7 +2,7 @@ import sys
 import requests
 
 if len(sys.argv) < 3 or len(sys.argv) > 4:
-    exit("Usage: %s <URL> <PORT> [CA_FILE]" % sys.argv[0])
+    exit("Usage: %s <HOST> <PORT> [CA_FILE]" % sys.argv[0])
 
 host = sys.argv[1]
 port = sys.argv[2]
