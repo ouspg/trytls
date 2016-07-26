50aa4d0193ac38840031364c7d02e8c74557af49
aleksiklasila <aleksi.klasila35@gmail.com>
2016-07-26 12:56:17 +0300

    edited python-request

--- stubs/python-requests/run.py
+++ stubs/python-requests/run.py
@@ -1,6 +1,9 @@
 import sys
 import requests
 
+if len(sys.argv) < 3 or len(sys.argv) > 4:
+    exit("Usage: %s <URL> <PORT> [CA_FILE]" % sys.argv[0])
+
 host = sys.argv[1]
 port = sys.argv[2]
 verify = sys.argv[3] if len(sys.argv) > 3 else True
