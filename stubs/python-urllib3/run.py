44e573f81206ab1eedc1f2d0bcdd89b18bb66526
aleksiklasila <aleksi.klasila35@gmail.com>
2016-07-26 13:00:15 +0300

    edited python-urllib3

--- stubs/python-urllib3/run.py
+++ stubs/python-urllib3/run.py
@@ -2,7 +2,7 @@ import urllib3
 import sys
 import certifi
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 3 or len(sys.argv) > 4:
     exit("Usage: %s <URL> <PORT> [CA_FILE]" % sys.argv[0])
 
 cert = sys.argv[3] if len(sys.argv) > 3 else certifi.where()
