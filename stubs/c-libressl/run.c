95b79618cb4725577e13115a0d5d02806454faad
Ossi Herrala <oherrala@gmail.com>
2016-08-11 14:42:14 +0300

    Styling. Indentation is 8 spaces and braces around everything

--- stubs/c-libressl/run.c
+++ stubs/c-libressl/run.c
@@ -1,3 +1,5 @@
+/* -*- c-basic-offset: 8; indent-tabs-mode: nil; -*- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,88 +7,93 @@
 #include <err.h>
 
 int main(int argc, char **argv) {
-  struct tls_config *config;
-  struct tls *context;
-  char *host;
-  char *port;
-  char *ca_file = NULL;
-  int exit_value = EXIT_SUCCESS;
+        struct tls_config *config;
+        struct tls *context;
+        char *host;
+        char *port;
+        char *ca_file = NULL;
+        char write_buf[1024];
+        char read_buf[1024];
+        int exit_value = EXIT_SUCCESS;
 
-  char write_buf[1024];
-  char read_buf[1024];
+        write_buf[0] = '\0';
+        read_buf[0]  = '\0';
 
-  write_buf[0] = '\0';
-  read_buf[0] = '\0';
+        if (argc < 3 || argc > 4) {
+                printf("%s <host> <port> [ca-bundle]\n", argv[0]);
+                return EXIT_FAILURE;
+        }
 
-  if (argc < 3 || argc > 4) {
-    printf("%s <host> <port> [ca-bundle]\n", argv[0]);
-    return EXIT_FAILURE;
-  }
+        host = argv[1];
+        port = argv[2];
 
-  host = argv[1];
-  port = argv[2];
+        if (argc == 4) {
+                ca_file = argv[3];
+        }
 
-  if (argc == 4)
-    ca_file = argv[3];
+        if (tls_init() == -1) {
+                err(1, "tls_init");
+        }
 
-  if (tls_init() == -1)
-    err(1, "tls_init");
+        config = tls_config_new();
+        if (config == NULL)
+                err(1, "tls_config_new");
 
-  config = tls_config_new();
-  if (config == NULL)
-    err(1, "tls_config_new");
+        if (ca_file) {
+                if (tls_config_set_ca_file(config, ca_file) == -1) {
+                        err(1, "tls_config_set_ca_file");
+                }
+        }
 
-  if (ca_file) {
-    if (tls_config_set_ca_file(config, ca_file) == -1)
-      err(1, "tls_config_set_ca_file");
-    printf("Using CA bundle from %s\n", ca_file);
-  }
+        context = tls_client();
+        if (context == NULL) {
+                err(1, "tls_client");
+        }
 
-  context = tls_client();
-  if (context == NULL)
-    err(1, "tls_client");
+        if (tls_configure(context, config) == -1) {
+                err(1, "tls_configure");
+        }
 
-  if (tls_configure(context, config) == -1)
-    err(1, "tls_configure");
+        if (tls_connect(context, host, port) == -1) {
+                err(1, "tls_connect");
+        }
 
-  if (tls_connect(context, host, port) == -1)
-    err(1, "tls_connect");
+        if (tls_handshake(context) == -1) {
+                printf("%s\n", tls_error(context));
+                printf("REJECT\n");
+                exit_value = EXIT_SUCCESS;
+                goto cleanup;
+        }
 
-  if (tls_handshake(context) == -1) {
-    printf("%s\n", tls_error(context));
-    printf("REJECT\n");
-    exit_value = EXIT_SUCCESS;
-    goto cleanup;
-  }
+        /* Send HTTP GET request */
+        snprintf(write_buf, sizeof(write_buf),
+                 "%s\r\n%s: %s\r\n\r\n",
+                 "GET / HTTP/1.0",
+                 "Host", host);
+        if (tls_write(context, write_buf, strlen(write_buf)) == -1)
+                err(1, "tls_write");
 
-  /* Send HTTP GET request */
-  snprintf(write_buf, sizeof(write_buf),
-	   "%s\r\n%s: %s\r\n\r\n",
-	   "GET / HTTP/1.0",
-	   "Host", host);
-  if (tls_write(context, write_buf, strlen(write_buf)) == -1)
-    err(1, "tls_write");
+        /* Read HTTP GET response */
+        if (tls_read(context, read_buf, sizeof(read_buf)) == -1) {
+                err(1, "tls_read");
+        }
 
-  /* Read HTTP GET response */
-  if (tls_read(context, read_buf, sizeof(read_buf)) == -1)
-    err(1, "tls_read");
+        /* We only care about the first line from HTTP GET response */
+        for (int i=0; i < strlen(read_buf); i++) {
+                if (read_buf[i] == '\n') {
+                        read_buf[i] = '\0';
+                        break;
+                }
+        }
 
-  /* We only care about the first line from HTTP GET response */
-  for (int i=0; i < strlen(read_buf); i++) {
-    if (read_buf[i] == '\n') {
-      read_buf[i] = '\0';
-      break;
-    }
-  }
-
-  printf("%s\n", read_buf);
-  printf("ACCEPT\n");
-  exit_value = EXIT_SUCCESS;
+        printf("%s\n", read_buf);
+        printf("ACCEPT\n");
+        exit_value = EXIT_SUCCESS;
 
  cleanup:
-  tls_close(context);
-  tls_free(context);
-  tls_config_free(config);
+        tls_close(context);
+        tls_free(context);
+        tls_config_free(config);
 
-  return exit_value;
+        return exit_value;
 }
