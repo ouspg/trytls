ff800f11b948808e4574ec3a893ed4e259707dcf
Joachim Viide <jviide@iki.fi>
2016-08-17 15:51:57 +0300

    Make python2-urllib2 compatible with more Python 2.7 versions
    Try to catch ssl.CertificateError only if CertificateError is
    defined. Otherwise bail out by effectively doing a dummy
    "catch ():".

--- stubs/python2-urllib2/run.py
+++ stubs/python2-urllib2/run.py
@@ -11,7 +11,7 @@ cafile = sys.argv[3] if len(sys.argv) > 3 else None
 
 try:
     urllib2.urlopen("https://" + host + ":" + port, cafile=cafile)
-except ssl.CertificateError:
+except getattr(ssl, "CertificateError", ()):
     print("REJECT")
 except urllib2.URLError as exc:
     if not isinstance(exc.reason, ssl.SSLError):
