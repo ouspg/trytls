2b006811882f58d4db77bd698017beef7e8c6c59
Joachim Viide <jviide@iki.fi>
2016-08-25 05:46:10 +0300

    Fix flake8 errors

--- runners/trytls/main.py
+++ runners/trytls/main.py
@@ -5,10 +5,10 @@ _main = sys.modules.get("__main__", None)
 if _main is not None and not hasattr(_main, "__requires__"):
     _main.__requires__ = "trytls==" + __version__
 
-import pkg_resources
+import pkg_resources  # noqa
 
 for name in tuple(sys.modules):
     if name.split(".")[0] in ("pkg_resources", "setuptools"):
         del sys.modules[name]
 
-from runner import main
+from .runner import main  # noqa
